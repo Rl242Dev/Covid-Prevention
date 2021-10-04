15c35f3dc79337727fcaa532b891939071653165
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2021-10-04 20:38:29 +0200

    Updated, Fixed Bugs
    Finish

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ import time
 
 x = 'y'
 
-url = 'https://www.worldometers.info/coronavirus/'
-
 #Code Part ---------------------------
 while  x == 'y':
+    url = 'https://www.worldometers.info/coronavirus/'
+
     response = requests.get(url)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    x = soup.find('div', {"class":"maincounter-number"})
-    print(str(x))
+    soup = BeautifulSoup(response.content, 'html.parser')
+    x = soup.find('div', {"class":"maincounter-number"}).getText()
 
     toast = ToastNotifier()
-
-    toast.show_toast("Covid Prevention","In total " + x + "people have been infected by Covid-19; So put on your mask",duration=5,icon_path="Covid Prevention\Covid.ico")
-    time.sleep(5)
+    toast.show_toast("Covid Prevention","In total" + x + "people have been infected by Covid-19, So put on your mask" ,duration=10)
+    time.sleep(3000)
